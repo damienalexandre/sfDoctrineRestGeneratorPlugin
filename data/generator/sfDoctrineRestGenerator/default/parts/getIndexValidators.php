0770bd58009d5058047e1e52387ba853378f9262
Damien ALEXANDRE <mobman02@gmail.com>
2011-10-26 20:52:35 +0200

    Revert a wrong rename of some internal params

--- data/generator/sfDoctrineRestGenerator/default/parts/getIndexValidators.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/getIndexValidators.php
@@ -20,13 +20,13 @@ $max_items = $this->configuration->getValue('get.max_items'); ?>
       'max' => <?php echo $max_items ?>,
       'required' => false
     );
-    $validators['pagesize'] = new sfValidatorInteger($params);
+    $validators['page_size'] = new sfValidatorInteger($params);
 <?php endif; ?>
 <?php endif; ?>
 <?php $sort_custom = $this->configuration->getValue('get.sort_custom'); ?>
 <?php if ($sort_custom): ?>
-    $validators['orderby'] = new sfValidatorChoice(array('choices' => <?php echo var_export($this->table->getColumnNames()) ?>, 'required' => false));
-    $validators['sortorder'] = new sfValidatorChoice(array('choices' => array('asc', 'desc'), 'required' => false));
+    $validators['sort_by'] = new sfValidatorChoice(array('choices' => <?php echo var_export($this->table->getColumnNames()) ?>, 'required' => false));
+    $validators['sort_order'] = new sfValidatorChoice(array('choices' => array('asc', 'desc'), 'required' => false));
 <?php endif; ?>
 <?php $additional_params = $this->configuration->getValue('get.additional_params'); ?>
 <?php if ($additional_params): ?>
